b7e574dd3961f65d4f8d1ac938cfcdb08ee0c947
student <kmac.kimaru93@gmail.com>
2024-03-19 14:20:19 +0300

    adding arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random(1));
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const myNumbers = [10, 15, 20];
+const max = Math.max(...myNumbers);
